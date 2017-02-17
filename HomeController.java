ac5a77f87d8cd615cf1c2192d8fd741eb5b07387
Julia Chen Jing <juliachenjing@gmail.com>
2017-02-16 18:47:35 -0600

    Update HomeController.java

--- HomeController.java
+++ HomeController.java
@@ -1,1 +1,18 @@
+package com.packt.webstore.controller;
 
+import org.springframework.sterotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework..web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController{
+  
+  @RequestMapping("/")
+  public String welcome(Model model)
+  {
+    model.addAtribute("greeting","Welcome to Web Store");
+     model.addAtribute("tagline","The one and only amazing webstore");
+    
+    return "welcome";
+  }
+}
